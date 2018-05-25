724a7061f56d049a48f4b1f769f6382622644718
Your Name <you@example.com>
2018-05-25 10:33:17 +0200

    restarting work for final

--- script/compose/chapters.sh
+++ script/compose/chapters.sh
@@ -7,7 +7,10 @@ text="%%$1"
 
 for f in *[!.backup]
 do
-	text="$text\n\n\\input{section/$1/$f}\n\\\\FloatBarrier\n"
+	if [ -f "$f" ]
+	then
+		text="$text\n\n\\input{section/$1/$f}\n\\\\FloatBarrier\n"
+	fi
 done
 
 printf $text > ../$1.tex
